375850850a5057cd969e555c4506a0c435dbd2f0
Cameron Lay <cameronlay15@gmail.com>
2020-10-30 11:13:40 -0400

    export

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -127,3 +127,15 @@ const isLocalhost = Boolean(
             );
           });
       }
+
+      export function unregister() {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.ready
+            .then(registration => {
+              registration.unregister();
+            })
+            .catch(error => {
+              console.error(error.message);
+            });
+        }
+      }  
