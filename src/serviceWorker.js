e460a58c39bb0b81ef04adb62d07be391e25b0e9
Cameron Lay <cameronlay15@gmail.com>
2020-10-30 11:05:19 -0400

    event listner

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,3 +30,26 @@ const isLocalhost = Boolean(
         // serve assets; see https://github.com/facebook/create-react-app/issues/2374
         return;
       }
+
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  
+        if (isLocalhost) {
+          // This is running on localhost. Let's check if a service worker still exists or not.
+          checkValidServiceWorker(swUrl, config);
+  
+          // Add some additional logging to localhost, pointing developers to the
+          // service worker/PWA documentation.
+          navigator.serviceWorker.ready.then(() => {
+            console.log(
+              'This web app is being served cache-first by a service ' +
+                'worker. To learn more, visit https://bit.ly/CRA-PWA'
+            );
+          });
+        } else {
+          // Is not localhost. Just register service worker
+          registerValidSW(swUrl, config);
+        }
+      });
+    }
+  }
